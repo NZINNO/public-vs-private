4abe4204245cb044b9a7a97c2ee3e9ae93028fb3
Zinno <4070806@vtsd.com>
2019-11-05 08:02:52 -0500

    ch7

---
+++ Business.java
@@ -0,0 +1,8 @@
+public class Business{
+  public static void main(String arg[]){
+    Class compSci = newClass();
+
+
+    System.out.println(compSci.roomNumber());
+  }
+}
---
+++ Class.java
@@ -0,0 +1,17 @@
+public class Class{
+  int numComputers;
+  int roomNumber;
+  String className;
+  String subject;
+
+  public Class(){
+    numComputers = 20;
+    roomNumber = 222;
+    className = "Circus";
+    subject = "Good Question";
+  }
+
+  public int setroomNumber(int num){
+    roomNumber = num;
+  }
+}
